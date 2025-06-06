5670e18c7bfcdca70595cda4eb75487af08c2e83
kaiwen <kaiwen20010907@outlook.com>
2025-06-06 15:45:22 -0400

    m

--- temp.py
+++ temp.py
@@ -63,3 +63,9 @@ class WeightedSmoothL1Loss(nn.Module):
 
         # self.reduction == "mean"
         return loss_per_sample.sum() / (weight.sum() + self.eps)
+
+
+#  conda install cudatoolkit==11.8 -c nvidia
+#  pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu118
+# !pip install causal_conv1d==1.4.0
+# !pip install mamba_ssm==2.2.2

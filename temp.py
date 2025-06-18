1ec1f9171bca2b8a21b9aa0306db70e229c2392f
kaiwen <kaiwen20010907@outlook.com>
2025-06-18 10:22:30 -0400

    m

--- temp.py
+++ temp.py
@@ -2,3 +2,14 @@
  pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu118
 !pip install causal_conv1d==1.4.0
 !pip install mamba_ssm==2.2.2
+
+
+pip install torch==2.3.1+cu118 torchvision==0.18.1+cu118 torchaudio==2.3.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+
+# 4 ) install your project-specific libs
+pip install causal_conv1d==1.4.0 mamba_ssm==2.2.2
+
+
+!pip install torch==2.3.1+cu118 torchvision==0.18.1+cu118 torchaudio==2.3.1+cu118 \
+             --extra-index-url https://download.pytorch.org/whl/cu118
+!pip install causal_conv1d==1.4.0 mamba_ssm==2.2.2

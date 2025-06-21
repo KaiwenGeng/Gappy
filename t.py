c182edbedcf132d65d63e83dc1d596f03a5fbd24
kaiwen <kaiwen20010907@outlook.com>
2025-06-20 21:08:04 -0400

    asda

--- t.py
+++ t.py
@@ -1,6 +1,9 @@
-[
-    "bash -lc \"pip install --upgrade pip && "
-    "pip install torch==2.3.1+cu118 torchvision==0.18.1+cu118 "
-    "torchaudio==2.3.1+cu118 --index-url https://download.pytorch.org/whl/cu118 && "
-    "pip install causal_conv1d==1.4.0 mamba_ssm==2.2.2 --no-build-isolation\""
+start_lines = [
+    "bash -lc \"\
+pip install --upgrade pip setuptools wheel setuptools-scm && \
+pip install torch==2.3.1+cu118 torchvision==0.18.1+cu118 torchaudio==2.3.1+cu118 \
+            --index-url https://download.pytorch.org/whl/cu118 && \
+pip install causal_conv1d==1.4.0 --use-pep517 --no-binary :all: && \
+pip install mamba_ssm==2.2.2 --use-pep517 --no-build-isolation\
+\""
 ]

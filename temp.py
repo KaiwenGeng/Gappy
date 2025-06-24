f547257cba404bd9630e6668f2585441fb6b106f
KaiwenGeng <168481232+KaiwenGeng@users.noreply.github.com>
2025-06-23 21:04:26 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,20 +1,11 @@
-class ExoMixerGLU(nn.Module):
-    def __init__(self, n_exo, gate_bias=0.0, p_drop = 0.25):
+class UniPatchInput(nn.Module):
+    def __init__(self):
         super().__init__()
-        hidden = n_exo * 2
-        self.pre = nn.Linear(n_exo, hidden * 2)   # value | gate
-        nn.init.xavier_uniform_(self.pre.weight)
-        # set the biases: first half (value) = 0, second half (gate) = gate_bias
-        with torch.no_grad():
-            self.pre.bias[:hidden].zero_()
-            self.pre.bias[hidden:].fill_(gate_bias)
-
-        self.glu  = nn.GLU(dim=-1)
-        self.dropout = nn.Dropout(p_drop)
-        self.post = nn.Linear(hidden, 1)
-
-    def forward(self, exo_pred):                  # [bs,T,n_exo]
-        x = self.pre(exo_pred)
-        x = self.glu(x)                           # gate starts near 0
-        x = self.dropout(x) 
-        return self.post(x).squeeze(-1)     
+        self.proj = nn.Linear(5, 2)   # 5 exogenous → 2 channels
+        self.glu  = nn.GLU(dim=-1)    # → 1 channel (gated)
+    def forward(self, x):
+        endo = x[..., 5:6]            # endogenous
+        exo  = x[..., :5]             # exogenous
+        exo_mix = self.glu(self.proj(exo))
+        uni     = endo + exo_mix      # final [B, 3000, 121, 1]
+        return uni

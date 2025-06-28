d379e2e84afdb13de7142c77aa97cccb18a44533
KaiwenGeng <168481232+KaiwenGeng@users.noreply.github.com>
2025-06-27 22:06:37 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,9 +1,8 @@
 class TokenEmbedding(nn.Module):
     def __init__(self, c_in, d_model, embedding_kernel):
         super(TokenEmbedding, self).__init__()
-        padding = 1 if torch.__version__ >= '1.5.0' else 2
         self.tokenConv = nn.Conv1d(in_channels=c_in, out_channels=d_model,
-                                   kernel_size=embedding_kernel, padding=padding, padding_mode='circular', bias=False)
+                                   kernel_size=embedding_kernel, padding_mode='zeros', bias=False)
         for m in self.modules():
             if isinstance(m, nn.Conv1d):
                 nn.init.kaiming_normal_(
@@ -12,7 +11,6 @@ class TokenEmbedding(nn.Module):
     def forward(self, x):
         x = self.tokenConv(x.permute(0, 2, 1)).transpose(1, 2)
         return x
-
 
 
 

ee893e5178baace625b5b43d38e69a4e445287f8
KaiwenGeng <168481232+KaiwenGeng@users.noreply.github.com>
2025-07-11 12:37:06 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,12 +1,6 @@
-import torch
-import torchsort  # pip install torchsort
-
-def rank_gauss_soft(x, dim=0, eps=1e-6):
-    # Differentiable ranks
-    ranks = torchsort.soft_rank(x, dim=dim, regularization_strength=tau)
-
-    N = x.size(dim)
-    U = (ranks + 0.5) / (N + 1.0)
-    Z = torch.sqrt(torch.tensor(2.0, device=x.device)) \
-        * torch.special.erfinv(2 * U - 1)
-    return torch.clamp(Z, -3, 3)
+mlp = nn.Sequential(
+    nn.Linear(config.dmodel, hidden_dim),  # input → hidden
+    nn.GELU(),                             # non-linearity
+    nn.Dropout(drop_p),                    # dropout
+    nn.Linear(hidden_dim, config.c_out)    # hidden → output
+)
